d2ea71d3edbeb0695c5f484cf4d5dde979ae3dd8
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-01 17:03:30 -0800

    Messing around with stuff

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+import git
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!info'):
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
+        await client.send_message(message.channel, '```Project StarBot v0.0.1-{}\r\nDeveloped by CorpNewt and Sydney Erickson```'.format(sha))
+
+client.run('Mjg2NjUxNTk3NDIzOTAyNzIw.C5j0sw.qF968us3onhFSyjOgnv6-fr4_SU')
