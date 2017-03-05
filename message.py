766d7824a6e515e2572c11e522c03e7e1235b1c5
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-04 22:43:18 -0800

    Adding easy message creation function to message.py

--- message.py
+++ message.py
@@ -2,3 +2,9 @@ class message:
     command = None
     body = None
     file = None
+
+def create(body=None, file=None):
+    messageTo = message
+    messageTo.body = body
+    messageTo.file = file
+    return messageTo
