ceffdeb4cdc20027c0445fedf285ed5f799ce5d3
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-06 21:22:48 -0800

    Cache utils

---
+++ plugins/cacheutils.py
@@ -0,0 +1,29 @@
+import plugin
+import command
+import message
+import main
+
+import glob
+
+def onInit(plugin_in):
+    cachecount_command = command.command(plugin_in, 'cachecount', shortdesc='Count the number of cached items for a command')
+    caches_command =     command.command(plugin_in, 'caches',     shortdesc='Count the number of cached items per command')
+    totalcache_command = command.command(plugin_in, 'totalcache', shortdesc='Count the total amount of cached items')
+    return plugin.plugin(plugin_in, 'cacheutils', [cachecount_command, caches_command, totalcache_command])
+
+def onCommand(message_in):
+    if message_in.command == 'cachecount':
+        if message_in.body == '':
+            return message.message(body='No plugin specified')
+        return message.message(body='```{}```'.format(len(glob.glob('cache/{}_*'.format(message_in.body.strip())))))
+
+    if message_in.command == 'caches':
+        cacheString = ''
+        for command in main.commands:
+            commandCacheSize = len(glob.glob('cache/{}_*'.format(command.name)))
+            if commandCacheSize > 0:
+                cacheString += '{} - {}\n'.format(command.name, commandCacheSize)
+        return message.message(body='```{}```'.format(cacheString))
+
+    if message_in.command == 'totalcache':
+        return message.message(body='```{}```'.format(len(glob.glob('cache/*'))))
