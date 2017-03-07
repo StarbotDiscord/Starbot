d547fb6cece25f38245813d58191a6bfe961b204
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-06 16:48:02 -0800

    TinyURL message API compliance fixes

--- plugins/tinyurl.py
+++ plugins/tinyurl.py
@@ -8,4 +8,4 @@ def onInit(plugin):
     return plugin.plugin.plugin(plugin, 'tinyurl', [tinyurl_command])
 
 def onCommand(message_in):
-    return message.create(body=urlopen("http://tinyurl.com/api-create.php?url=" + message_in.body.strip()).read().decode("utf-8"))
+    return message.message(body=urlopen("http://tinyurl.com/api-create.php?url=" + message_in.body.strip()).read().decode("utf-8"))
