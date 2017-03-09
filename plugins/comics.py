ec431ede3e1e36cd64d82641c396a283bda64f3d
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-09 14:21:55 -0800

    More XKCD caching fixes

--- plugins/comics.py
+++ plugins/comics.py
@@ -20,31 +20,11 @@ def onCommand(message_in):
             except:
                 return message.message(body='Input of `{}` is not a valid number'.format(message_in.body))
 
-            json_filename = 'cache/xkcd_{}.json'.format(message_in.body.strip())
-
-            if os.path.isfile(json_filename):
-                pass
-            else:
-                print("Grabbing XKCD JSON! Not in cache.")
-                try:
-                    urllib.request.urlretrieve("https://xkcd.com/{}/info.0.json".format(message_in.body.strip()), json_filename)
-                except urllib.error.HTTPError as e:
-                    return message.message(body='Could not find comic with ID `{}`'.format(message_in.body))
-                except urllib.error.URLError as e:
-                    return message.message(body='There was an issue connecting to XKCD'.format(message_in.body))
-
-            f = ''
-            with open(json_filename) as m:
-                f = m.read()
-            data = json.loads(f)
+            data = json.loads(caching.getJson("https://xkcd.com/{}/info.0.json".format(message_in.body.strip())))
         else:
-            try:
-                f = urllib.request.urlopen("https://xkcd.com/info.0.json")
-            except urllib.error.URLError as e:
-                return message.message(body='There was an issue connecting to XKCD'.format(message_in.body))
-            data = json.load(f)
+            data = json.loads(caching.getJson("https://xkcd.com/info.0.json"))
 
-        caching.downloadToCache(data['img'], '{}.png'.format(['num']))
+        caching.downloadToCache(data['img'], '{}.png'.format(data['num']), caller='xkcd')
 
         return message.message(body='**{}/{}/{} - {}**\n_{}_'.format(data['month'], data['day'], data['year'], data['safe_title'], data['alt']),
                               file='cache/xkcd_{}.png'.format(data['num']))
