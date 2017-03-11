d24a6a45aaa7312f3543320aeec091f9f03eaef6
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-10 18:05:32 -0800

    Basic command unittests

---
+++ tests/test_command.py
@@ -0,0 +1,47 @@
+import unittest
+import command
+
+class TestCommandSuite(unittest.TestCase):
+
+    def testCommandBasicArgs(self):
+        commandTest = command.command(unittest, 'testplugin')
+        self.assertEqual(commandTest.name, 'testplugin')
+        self.assertEqual(commandTest.plugin, unittest)
+        self.assertEqual(commandTest.shortdesc, 'no description')
+        self.assertEqual(commandTest.devcommand, False)
+
+    def testCommandOverwrite(self):
+        commandTest = command.command(unittest, 'testplugin')
+        self.assertEqual(commandTest.name, 'testplugin')
+        self.assertEqual(commandTest.plugin, unittest)
+        self.assertEqual(commandTest.shortdesc, 'no description')
+        self.assertEqual(commandTest.devcommand, False)
+
+        commandTest = command.command(unittest, 'testplugin', shortdesc='python testing is fun!')
+        self.assertEqual(commandTest.name, 'testplugin')
+        self.assertEqual(commandTest.plugin, unittest)
+        self.assertEqual(commandTest.shortdesc, 'python testing is fun!')
+        self.assertEqual(commandTest.devcommand, False)
+
+    def testCommandArgs(self):
+        commandTest = command.command(unittest, 'testplugin', shortdesc='python testing is fun!', devcommand=True)
+        self.assertEqual(commandTest.name, 'testplugin')
+        self.assertEqual(commandTest.plugin, unittest)
+        self.assertEqual(commandTest.shortdesc, 'python testing is fun!')
+        self.assertEqual(commandTest.devcommand, True)
+
+    def testCommandChangeArgs(self):
+        commandTest = command.command(unittest, 'testplugin', shortdesc='python testing is fun!')
+        self.assertEqual(commandTest.name, 'testplugin')
+        self.assertEqual(commandTest.plugin, unittest)
+        self.assertEqual(commandTest.shortdesc, 'python testing is fun!')
+        self.assertEqual(commandTest.devcommand, False)
+
+        commandTest.shortdesc = 'We\'ve changed the description!'
+        self.assertEqual(commandTest.name, 'testplugin')
+        self.assertEqual(commandTest.plugin, unittest)
+        self.assertEqual(commandTest.shortdesc, 'We\'ve changed the description!')
+        self.assertEqual(commandTest.devcommand, False)
+
+if __name__ == '__main__':
+    unittest.main()
