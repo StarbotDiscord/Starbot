79584059d5f87be52576a90a976dd296b9babfac
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-22 17:12:35 -0700

    Adding bot owner internal roles to the API

--- api/db.py
+++ api/db.py
@@ -33,3 +33,37 @@ def logUserMessage(message):
               (message.author.id, message.author.name, message.content, message.server.id, message.server.name))
     conn.commit()
     conn.close()
+
+def isOwner(uid):
+    conn = sqlite3.connect("bot.db3")
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS owners (userid INTEGER)')
+    cur = c.execute('SELECT userid FROM owners')
+    for row in list(cur):
+        if str(row[0]) == str(uid):
+            conn.commit()
+            conn.close()
+            return True
+    conn.commit()
+    conn.close()
+    return False
+
+def getOwners():
+    conn = sqlite3.connect("bot.db3")
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS owners (userid INTEGER)')
+    cur = c.execute('SELECT userid FROM owners')
+    owners = []
+    for row in list(cur):
+        owners.append(row[0])
+    conn.commit()
+    conn.close()
+    return owners
+
+def addOwner(uid):
+    conn = sqlite3.connect("bot.db3")
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS owners (userid INTEGER)')
+    c.execute('INSERT INTO owners VALUES (' + uid + ')')
+    conn.commit()
+    conn.close()
