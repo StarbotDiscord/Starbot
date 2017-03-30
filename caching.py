977a70f8117081651fda2c0cf99f6aaa6e908a09
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-29 23:36:39 -0700

    Let's use the correct function arguments, shall we?

--- caching.py
+++ caching.py
@@ -30,7 +30,7 @@ def getJson(url, caller='', customName='', save=True):
     else:
         jsonString = urllib.request.urlopen(url).read().decode("utf-8")
         if save == True:
-            writeString(jsonString)
+            writeString(jsonString, caller, fullFilename)
         return jsonString
 
 
