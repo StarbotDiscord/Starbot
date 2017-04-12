f72869bc2891284fe69f8a3f0a9695c9e756dbea
Sydney Erickson <sydneyerickson@icloud.com>
2017-04-12 14:16:11 -0700

    Create a git api with methods to get lastest commit and branch

---
+++ api/git.py
@@ -0,0 +1,14 @@
+def getCommit():
+    with open(".git/HEAD") as f:
+        HEADFileS = f.read()
+        HFileA = HEADFileS.split(": ")
+        with open(".git/{}".format(HFileA[1].strip())) as e:
+            commit = e.read()
+            return commit
+
+def getBranch():
+    with open(".git/HEAD") as f:
+        HEADFileS = f.read()
+        HFileA = HEADFileS.split(": ")
+        branch = HFileA[1].split("/")[-1]
+        return branch
