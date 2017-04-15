8d899ef85bcb5c34c16c4a11bdc91f25543acf92
John Davis <goldfishnet@aol.com>
2017-04-15 14:19:02 -0500

    Fixed Windows script

--- run.bat
+++ run.bat
@@ -3,10 +3,11 @@
 REM StarBot run script for Windows.
 
 REM Get latest version and star bot.
+:update
 echo Updating...
 git pull
 echo Starting bot...
 python main.py
 
 REM Loop back again.
-goto start
+goto update
