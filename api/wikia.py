215e02ea7a5aecd73fcef39f7aac3e6622dcf906
Sydney Erickson <sydneyerickson@icloud.com>
2017-05-08 21:03:05 -0700

    Fix Wikia API to not be limited to SVTFOE Wikia

--- api/wikia.py
+++ api/wikia.py
@@ -11,7 +11,7 @@ class Wikia:
         # Search for a page on Wikia
         # TODO: make limits over 1 return an array
         searchTerm = term.replace(" ", "+")
-        url = "http://starvstheforcesofevil.wikia.com/api/v1/Search/List?query={}&limit=1&minArticleQuality=10&batch=1&namespaces=0%2C14".format(searchTerm)
+        url = "http://{}.wikia.com/api/v1/Search/List?query={}&limit=1&minArticleQuality=10&batch=1&namespaces=0%2C14".format(self.wiki_name, searchTerm)
 
         jsonString = urllib.request.urlopen(url).read().decode("utf-8")
         json_d = json.loads(jsonString)
@@ -20,7 +20,7 @@ class Wikia:
 
     def getPage(self, page_id):
         # Get a page on Wikia based on the page ID
-        url = "http://starvstheforcesofevil.wikia.com/api/v1/Articles/AsSimpleJson?id={}".format(page_id)
+        url = "http://{}.wikia.com/api/v1/Articles/AsSimpleJson?id={}".format(self.wiki_name, page_id)
 
         jsonString = urllib.request.urlopen(url).read().decode("utf-8")
         json_d = json.loads(jsonString)
