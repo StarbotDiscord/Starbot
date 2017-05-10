2ce95684345c8925496bb5ab040272e0b8494fd2
Sydney Erickson <sydneyerickson@icloud.com>
2017-05-10 16:58:42 -0700

    :thinking: Calc testing fixes

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -16,29 +16,29 @@ import unittest
 from api import message, plugin
 from plugins import calc
 
+import asyncio
+
 class TestFunSuite(unittest.TestCase):
 
     def testCalcImport(self):
         result = calc.onInit(__import__('api.plugin'))
         self.assertEqual(type(result), plugin.plugin)
 
-
-
     def testCalcEmptyEqu(self):
         msg = message.message(body="")
         msg.command = "calc"
-        result = calc.onCommand(msg)
+        result = yield from calc.onCommand(msg)
         self.assertEqual(type(result), type(msg))
 
     def testCalcInvalEqu(self):
         msg = message.message(body="banana")
         msg.command = "calc"
-        result = calc.onCommand(msg)
+        result = yield from calc.onCommand(msg)
         self.assertEqual(type(result), type(msg))
 
     def testCalcValidEqu(self):
         msg = message.message(body="3+4")
         msg.command = "calc"
-        result = calc.onCommand(msg)
+        result = yield from calc.onCommand(msg)
         self.assertEqual(type(result), type(msg))
         self.assertEqual(result.body, "`3+4` = `7.0`")
