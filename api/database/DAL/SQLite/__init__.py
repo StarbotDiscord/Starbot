872849f9c013c957c8778bb8143e4d62dc0c4b14
Oxylibrium <akashthenerd@gmail.com>
2017-05-10 17:59:03 +0530

    PEP Compliance for DAL

--- api/database/DAL/SQLite/__init__.py
+++ api/database/DAL/SQLite/__init__.py
@@ -1,93 +1,92 @@
 #    Copyright 2017 Starbot Discord Project
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+"""SQLite Abstraction layer for Starbot's database API."""
 
 import sqlite3
 from api.database.entry import entry
 
 
-def open(db_in):
-    # Open the database.
+def db_open(db_in):
+    """Open the Database."""
     db_in.type = "SQLite"
     db_in.connection = sqlite3.connect("bot.db3")
 
 
-def close(db_in):
+def db_close(db_in):
+    """Close the Database."""
     # Close the database.
     db_in.connection.close()
 
 
-def createTableIfNotExist(db_in, tablename):
-    # Create a new table in the database unless it already exists.
-    c = db_in.connection.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS %s(id INTEGER PRIMARY KEY);' % tablename)
+def db_create_table(db_in, tablename):
+    """Create a new table in the database unless it already exists."""
+    connection = db_in.connection.cursor()
+    connection.execute('CREATE TABLE IF NOT EXISTS %s(id INTEGER PRIMARY KEY);' % tablename)
 
 
-def insertToDatabase(db_in, table, dict_in):
-    # Create new entry in database.
-    c = db_in.connection.cursor()
+def db_insert(db_in, table, dict_in):
+    """Insert new entry in database."""
+    connection = db_in.connection.cursor()
     keys = []
     values = []
     for key, value in dict_in.items():
         keys.append(key)
-        values.append("'" + value.replace("'", "''") + "'")
+        # Escape quotes
+        values.append("'" + str(value.replace("'", "''")) + "'")
 
     # Update entries for each key and value.
     for key in keys:
         # Attempt to add column, fail silently if it exists.
         try:
-            c.execute('ALTER TABLE %s ADD COLUMN %s' % (table.name, key))
-        except:
+            connection.execute('ALTER TABLE %s ADD COLUMN %s' % (table.name, key))
+        except sqlite3.OperationalError:
             pass
 
-    c.execute('INSERT INTO %s(%s) VALUES (%s);' % (table.name, ",".join(keys), ",".join(values)))
-    return_entry = entry(c.lastrowid, db_in, table, dict_in)
+    connection.execute('INSERT INTO %s(%s) VALUES (%s);' % (table.name, ",".join(keys), ",".join(values)))
+    return_entry = entry(connection.lastrowid, db_in, table, dict_in)
     db_in.connection.commit()
     return return_entry
 
 
-def editInDatabase(db_in, table, id, dict_in):
-    # Edit existing database entry.
-    c = db_in.connection.cursor()
-    keys = []
-    values = []
-    tableArray = []
+def db_entry_edit(db_in, table, entry_id, dict_in):
+    """Edit existing database entry."""
+    connection = db_in.connection.cursor()
 
     # Update entries for each key and value.
     for key, value in dict_in.items():
         # Attempt to add column, fail silently if it exists.
         try:
-            c.execute('ALTER TABLE %s ADD COLUMN %s' % (table.name, key.replace("'", "''")))
-        except:
+            connection.execute('ALTER TABLE %s ADD COLUMN %s' % (table.name, key.replace("'", "''")))
+        except sqlite3.OperationalError:
             pass
         # Update the entry in the database.
-        c.execute("UPDATE '%s' SET %s='%s' WHERE id=%s;" %
-                  (table.name, key, value, str(id)))
+        connection.execute("UPDATE '%s' SET %s='%s' WHERE id=%s;" % (table.name, key, value, str(entry_id)))
 
     db_in.connection.commit()
 
 
-def deleteEntryInDatabase(db_in, table, id):
-    # Delete database entry.
-    c = db_in.connection.cursor()
-    c.execute('DELETE FROM %s WHERE id=%s' % (table.name, str(id)))
+def db_entry_delete(db_in, table, entry_id):
+    """Delete database entry."""
+    connection = db_in.connection.cursor()
+    connection.execute('DELETE FROM %s WHERE id=%s' % (table.name, str(entry_id)))
 
 
-def searchInTable(db_in, table, searchTerm, searchFor):
-    # Select first entry that matches and return type entry.
-    c = db_in.connection.cursor()
-    cursor = c.execute("SELECT * FROM %s WHERE %s='%s';" % (table.name, searchTerm, searchFor.replace("'", "''")))
+def db_search(db_in, table, search_key, search_query):
+    """Select first entry that matches and return type entry."""
+    connection = db_in.connection.cursor()
+    search_query = search_query.replace("'", "''")  # Escape quotes
+    cursor = connection.execute("SELECT * FROM %s WHERE %s='%s';" % (table.name, search_key, search_query))
     for row in cursor:
         print(row[0])
-        newEntry = entry(row[0], db_in, table, row)
-        return newEntry
+        return entry(row[0], db_in, table, row)
--- api/database/DAL/__init__.py
+++ api/database/DAL/__init__.py
@@ -15,18 +15,18 @@
 from api.database.DAL import SQLite
 from api.database.db import db
 
-def open(db_in):
+def db_open(db_in):
     if db.type == "SQLite":
-        SQLite.open(db_in)
+        SQLite.db_open(db_in)
 
-def close(db_in):
+def db_close(db_in):
     if db.type == "SQLite":
         SQLite.close(db_in)
 
-def createTableIfNotExist(db_in, tablename):
+def db_create_table(db_in, tablename):
     if db.type == "SQLite":
-        SQLite.createTableIfNotExist(db_in, tablename)
+        SQLite.db_create_table(db_in, tablename)
 
-def insertToDatabase(db_in, table, dict_in):
+def db_insert(db_in, table, dict_in):
     if db.type == "SQLite":
-        return SQLite.insertToDatabase(db_in, table, dict_in)
+        return SQLite.db_insert(db_in, table, dict_in)
