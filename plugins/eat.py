c354ed5851cacaec2137dc41f862dd13f2d6b112
Oxylibrium <akashthenerd@gmail.com>
2017-05-12 01:49:13 -0700

    Use only one return statement

--- plugins/eat.py
+++ plugins/eat.py
@@ -1,17 +1,17 @@
 #    Copyright 2017 Starbot Discord Project
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-
+'''The eat command.'''
 import random
 
 from api import command, message, plugin
@@ -20,26 +20,23 @@ from libs import displayname
 
 # Eating plugin
 def onInit(plugin_in):
+    '''List commands for plugin.'''
     eat_command = command.command(plugin_in, 'eat', shortdesc='Eat someone or something')
     return plugin.plugin(plugin_in, 'eat', [eat_command])
 
 async def onCommand(message_in):
+    '''Run plugin commands.'''
     if message_in.command == 'eat':
         author = displayname.name(message_in.author)
         member = message_in.body.strip()
+        mem_check = displayname.memberForName(member, message_in.server)
 
         # Check if we're eating nothing
         if member == "":
-            list_none = ['you sit quietly and eat *nothing*...',
-                         'you\'re *sure* there was something to eat, so you just chew on nothingness...',
-                         'there comes a time when you need to realize that you\'re just chewing nothing for the sake of chewing.  That time is now.']
-
-            randnum = random.randint(0, len(list_none) - 1)
-            return message.message('*{}*, {}'.format(author, list_none[randnum]))
-
-        # Check if we're eating a member
-        mem_check = displayname.memberForName(member, message_in.server)
-        if mem_check:
+            msg_list = ['you sit quietly and eat *nothing*...',
+                        'you\'re *sure* there was something to eat, so you just chew on nothingness...',
+                        'there comes a time when you need to realize that you\'re just chewing nothing for the sake of chewing.  That time is now.']
+        elif mem_check:
             # We're eating a member - let's do a bot-check
             if mem_check.id == message_in.server.me.id:
                 # It's me!
@@ -64,16 +61,12 @@ async def onCommand(message_in):
                             'you take a quick bite out of *{}*.  They probably didn\'t even notice.'.format(mem_name),
                             'you sink your teeth into *{}\'s* shoulder - they turn to face you, eyes wide as you try your best to scurry away and hide.'.format(mem_name),
                             'your jaw clamps down on *{}* - a satisfying *crunch* emanates as you finish your newest meal.'.format(mem_name)]
+        else:
+            msg_list = ['you take a big chunk out of *{}*. *Delicious.*'.format(member),
+                        'your teeth sink into *{}* - it tastes satisfying.'.format(member),
+                        'you rip hungrily into *{}*, tearing it to bits!'.format(member),
+                        'you just can\'t bring yourself to eat *{}* - so you just hold it for awhile...'.format(member),
+                        'you attempt to bite into *{}*, but you\'re clumsier than you remember - and fail...'.format(member)]
 
-            randnum = random.randint(0, len(msg_list) - 1)
-            return message.message('*{}*, {}'.format(author, msg_list[randnum]))
-
-        # Assume we're eating something else
-        itemList = ['you take a big chunk out of *{}*. *Delicious.*'.format(member),
-                    'your teeth sink into *{}* - it tastes satisfying.'.format(member),
-                    'you rip hungrily into *{}*, tearing it to bits!'.format(member),
-                    'you just can\'t bring yourself to eat *{}* - so you just hold it for awhile...'.format(member),
-                    'you attempt to bite into *{}*, but you\'re clumsier than you remember - and fail...'.format(member),]
-
-        randnum = random.randint(0, len(itemList) - 1)
-        return message.message('*{}*, {}'.format(author, itemList[randnum]))
+        randnum = random.randint(0, len(msg_list) - 1)
+        return message.message('*{}*, {}'.format(author, msg_list[randnum]))
