5167ae9c0864b4a9f35ed8a35d676babbf144c3f
Oxylibrium <akashthenerd@gmail.com>
2017-05-12 02:15:13 -0700

    Refactor fun.py

--- plugins/fun.py
+++ plugins/fun.py
@@ -1,17 +1,17 @@
 #    Copyright 2017 Starbot Discord Project
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-
+'''Random fun and emoji commands.'''
 import random
 
 from api import command, message, plugin
@@ -19,6 +19,7 @@ from api import command, message, plugin
 
 # Fun plugin
 def onInit(plugin_in):
+    '''List commands for plugin.'''
     lenny_command = command.command(plugin_in, 'lenny', shortdesc='Give some Lenny')
     shrug_command = command.command(plugin_in, 'shrug', shortdesc='Shrug it off')
     tableflip_command = command.command(plugin_in, 'tableflip', shortdesc='Flip a table')
@@ -27,6 +28,7 @@ def onInit(plugin_in):
     return plugin.plugin(plugin_in, 'fun', [lenny_command, shrug_command, tableflip_command, fart_command, beta_command])
 
 async def onCommand(message_in):
+    '''Run plugin commands.'''
     # Lenny.
     if message_in.command == 'lenny':
         # Create message.
@@ -42,7 +44,7 @@ async def onCommand(message_in):
     # Shrug.
     if message_in.command == 'shrug':
         # Create message.
-        msg = "¯\_(ツ)_/¯"
+        msg = r"¯\_(ツ)_/¯"
 
         # Append extra on if needed.
         if message_in.body.strip():
@@ -66,9 +68,9 @@ async def onCommand(message_in):
     # Fart.
     if message_in.command == 'fart':
         # Make farts.
-        fartList = ["Poot", "Prrrrt", "Thhbbthbbbthhh", "Plllleerrrrffff", "Toot", "Blaaaaahnk", "Squerk"]
-        randnum = random.randint(0, len(fartList)-1)
-        msg = '{}'.format(fartList[randnum])
+        fart_list = ["Poot", "Prrrrt", "Thhbbthbbbthhh", "Plllleerrrrffff", "Toot", "Blaaaaahnk", "Squerk"]
+        randnum = random.randint(0, len(fart_list)-1)
+        msg = '{}'.format(fart_list[randnum])
 
         # Append extra on if needed.
         if message_in.body.strip():
