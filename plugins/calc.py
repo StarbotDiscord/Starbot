d09535102f6760c1e381b9064a2bd6fe93567112
Oxylibrium <akashthenerd@gmail.com>
2017-05-11 23:30:13 -0700

    Refactor calc.py, spacing

--- plugins/calc.py
+++ plugins/calc.py
@@ -22,27 +22,24 @@ from pyparsing import (Literal, CaselessLiteral, Word, Combine, Group, Optional,
 
 from api import command, message, plugin
 
-__author__='Paul McGuire'
+__author__ = 'Paul McGuire'
 __version__ = '$Revision: 0.0 $'
 __date__ = '$Date: 2009-03-20 $'
-__source__='''http://pyparsing.wikispaces.com/file/view/fourFn.py
+__source__ = '''http://pyparsing.wikispaces.com/file/view/fourFn.py
 http://pyparsing.wikispaces.com/message/view/home/15549426
 '''
-__note__='''
+__note__ = '''
 All I've done is rewrap Paul McGuire's fourFn.py as a class, so I can use it
 more easily in other places.
 '''
 
 class NumericStringParser(object):
-    '''
-    Most of this code comes from the fourFn.py pyparsing example
-
-    '''
-    def pushFirst(self, strg, loc, toks ):
-        self.exprStack.append( toks[0] )
-    def pushUMinus(self, strg, loc, toks ):
-        if toks and toks[0]=='-':
-            self.exprStack.append( 'unary -' )
+    '''Most of this code comes from the fourFn.py pyparsing example.'''
+    def push_first(self, strg, loc, toks):
+        self.expression_stack.append(toks[0])
+    def push_unary_minus(self, strg, loc, toks):
+        if toks and toks[0] == '-':
+            self.expression_stack.append('unary -')
     def __init__(self):
         """
         expop   :: '^'
@@ -54,74 +51,77 @@ class NumericStringParser(object):
         term    :: factor [ multop factor ]*
         expr    :: term [ addop term ]*
         """
-        point = Literal( "." )
-        e     = CaselessLiteral( "E" )
-        fnumber = Combine( Word( "+-"+nums, nums ) +
-                        Optional( point + Optional( Word( nums ) ) ) +
-                        Optional( e + Word( "+-"+nums, nums ) ) )
+        point = Literal(".")
+        exp = CaselessLiteral("E")
+        fnumber = Combine(Word("+-" + nums, nums) +
+                          Optional(point + Optional(Word(nums))) +
+                          Optional(exp + Word("+-" + nums, nums)))
         ident = Word(alphas, alphas+nums+"_$")
-        plus  = Literal( "+" )
-        minus = Literal( "-" )
-        mult  = Literal( "x" )
-        div   = Literal( "/" )
-        lpar  = Literal( "(" ).suppress()
-        rpar  = Literal( ")" ).suppress()
-        addop  = plus | minus
+        plus = Literal("+")
+        minus = Literal("-")
+        mult = Literal("x")
+        div = Literal("/")
+        lpar = Literal("(").suppress()
+        rpar = Literal(")").suppress()
+        addop = plus | minus
         multop = mult | div
-        expop = Literal( "^" )
-        pi    = CaselessLiteral( "PI" )
+        powop = Literal("^")
+        pi = CaselessLiteral("PI")
         expr = Forward()
         atom = ((Optional(oneOf("- +")) +
-                (pi|e|fnumber|ident+lpar+expr+rpar).setParseAction(self.pushFirst))
+                 (pi|exp|fnumber|ident+lpar+expr+rpar).setParseAction(self.push_first))
                 | Optional(oneOf("- +")) + Group(lpar+expr+rpar)
-                ).setParseAction(self.pushUMinus)
+               ).setParseAction(self.push_unary_minus)
         # by defining exponentiation as "atom [ ^ factor ]..." instead of
         # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
         # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
         factor = Forward()
-        factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( self.pushFirst ) )
-        term = factor + ZeroOrMore( ( multop + factor ).setParseAction( self.pushFirst ) )
-        expr << term + ZeroOrMore( ( addop + term ).setParseAction( self.pushFirst ) )
+        factor << atom + ZeroOrMore((powop + factor).setParseAction(self.push_first))
+        term = factor + ZeroOrMore((multop + factor).setParseAction(self.push_first))
+        expr << term + ZeroOrMore((addop + term).setParseAction(self.push_first))
         # addop_term = ( addop + term ).setParseAction( self.pushFirst )
         # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
         # expr <<  general_term
         self.bnf = expr
         # map operator symbols to corresponding arithmetic operations
         epsilon = 1e-12
-        self.opn = { "+" : operator.add,
-                "-" : operator.sub,
-                "x" : operator.mul,
-                "/" : operator.truediv,
-                "^" : operator.pow }
-        self.fn  = { "sin" : math.sin,
-                "cos" : math.cos,
-                "tan" : math.tan,
-                "abs" : abs,
-                "trunc" : lambda a: int(a),
-                "round" : round,
-                "sgn" : lambda a: abs(a)>epsilon and cmp(a,0) or 0}
-    def evaluateStack(self, s ):
-        op = s.pop()
-        if op == 'unary -':
-            return -self.evaluateStack( s )
-        if op in "+-x/^":
-            op2 = self.evaluateStack( s )
-            op1 = self.evaluateStack( s )
-            return self.opn[op]( op1, op2 )
-        elif op == "PI":
+        self.opn = {"+" : operator.add,
+                    "-" : operator.sub,
+                    "x" : operator.mul,
+                    "/" : operator.truediv,
+                    "^" : operator.pow}
+        self.function = {"sin" : math.sin,
+                         "cos" : math.cos,
+                         "tan" : math.tan,
+                         "abs" : abs,
+                         "trunc" : lambda a: int(a),
+                         "round" : round,
+                         "sgn" : lambda a: abs(a) > epsilon and cmp(a, 0) or 0}
+
+    def stack_evaluate(self, s):
+        '''Backend stack evaluation'''
+        op_eval = s.pop()
+        if op_eval == 'unary -':
+            return -self.stack_evaluate(s)
+        if op_eval in "+-x/^":
+            op_eval2 = self.stack_evaluate(s)
+            op_eval1 = self.stack_evaluate(s)
+            return self.opn[op_eval](op_eval1, op_eval2)
+        elif op_eval == "PI":
             return math.pi # 3.1415926535
-        elif op == "E":
+        elif op_eval == "E":
             return math.e  # 2.718281828
-        elif op in self.fn:
-            return self.fn[op]( self.evaluateStack( s ) )
-        elif op[0].isalpha():
+        elif op_eval in self.function:
+            return self.function[op_eval](self.stack_evaluate(s))
+        elif op_eval[0].isalpha():
             return 0
         else:
-            return float( op )
-    def eval(self,num_string,parseAll=True):
-        self.exprStack=[]
-        results=self.bnf.parseString(num_string,parseAll)
-        val=self.evaluateStack( self.exprStack[:] )
+            return float(op_eval)
+
+    def stack_eval(self, num_string, parse_all=True):
+        self.expression_stack = []
+        results = self.bnf.parseString(num_string, parse_all)
+        val = self.stack_evaluate(self.expression_stack[:])
         return val
 
 def onInit(plugin_in):
@@ -129,7 +129,7 @@ def onInit(plugin_in):
     calc_command = command.command(plugin_in, 'calc', shortdesc='Calculate given input')
     return plugin.plugin(plugin_in, 'calc', [calc_command])
 
-async def onCommand(message_in):
+def onCommand(message_in):
     """Do some math."""
     formula = message_in.body
     formula = formula.replace('*', 'x')
@@ -140,7 +140,7 @@ async def onCommand(message_in):
 
     try:
         nsp = NumericStringParser()
-        answer = nsp.eval(formula)
+        answer = nsp.stack_eval(formula)
     except Exception as e:
         print("CALC PLUGIN EXCEPTION\r\n{}".format(e))
         msg = 'I couldn\'t parse "{}" :(\n\n'.format(formula)
